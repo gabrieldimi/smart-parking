c3055d7774504c8c542f553680470ffa9443b145
gabrieldimi <gabrieldimi69@gmail.com>
2019-07-09 17:14:57 +0200

    usin pigpio now

--- Pi/parking_spot.py
+++ Pi/parking_spot.py
@@ -1,6 +1,6 @@
-import RPi.GPIO as GPIO
+import pigpio
 import paho.mqtt.client as mqtt
-import time, sys, os, json, threading, uuid
+import time, sys, os, json, threading, uuid, sonar_ranger
 from datetime import datetime
 
 class UUIDEncoder(json.JSONEncoder):
@@ -10,45 +10,37 @@ class UUIDEncoder(json.JSONEncoder):
 		return json.JSONEncoder.default(self,object)
 
 class myThread (threading.Thread):
-	def __init__(self, threadID, name, yellow_pin, blue_pin):
+	def __init__(self, threadID, name):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = name
-		self.yellow_pin = yellow_pin
-		self.blue_pin = blue_pin
 		self.thread_working = True
 		self.do_blink = False
 		self.taken = False
 		self.alpr_recognition_done = False
 	def run(self):
-		blink(self.yellow_pin,self.blue_pin)
+		blink()
 
-def blink(yellow_pin,blue_pin):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(yellow_pin, GPIO.OUT)
-	GPIO.output(yellow_pin, GPIO.LOW)
-	GPIO.setup(blue_pin, GPIO.OUT)
-	GPIO.output(blue_pin, GPIO.LOW)
+def blink():
 	t = threading.currentThread()
 	while getattr(t,"thread_working"):
 
 		while getattr(t, "do_blink"):
-			GPIO.output(yellow_pin, GPIO.HIGH)
+			pi.write(yellow_gpio,1)
 			time.sleep(blink_speed)
-			GPIO.output(yellow_pin, GPIO.LOW)
+			pi.write(yellow_gpio,0)
 			time.sleep(blink_speed)
 
 		if getattr(t,"alpr_recognition_done") and getattr(t, "taken"):
-			GPIO.output(blue_pin,GPIO.HIGH)
+			pi.write(blue_gpio,1)
 		else:
-			GPIO.output(blue_pin,GPIO.LOW)
+			pi.write(blue_gpio,0)
 
 def on_message(client, userdata, message):
 	msg = str(message.payload.decode("utf-8"))
 	msg_as_json = json.loads(msg)
 	#print("Message topic:", message.topic)
 	try:
-
 		if msg_as_json['spot'] is not None:
 			if msg_as_json['spot'] == spot_number:
 				if message.topic == car_is_here:
@@ -71,38 +63,21 @@ def on_connect(client, userdata, flags, rc):
 	client.subscribe(image_is_taken)
 	client.subscribe(car_is_here)
 
-def pinsInitialization():
-	GPIO.setmode(GPIO.BOARD)
-	#Matching the pins to the GPIO slots
-	GPIO.setup(trigger_pin, GPIO.OUT)
-	GPIO.setup(echo_pin, GPIO.IN)
-	GPIO.setup(red_pin, GPIO.OUT)
-	GPIO.setup(green_pin, GPIO.OUT)
-	GPIO.setwarnings(False)
+def setToDefault(isBegin):
+	pi.write(red_gpio,0)
+	pi.write(yellow_gpio,0)
+	pi.write(blue_gpio,0)
+	if isBegin:
+		pi.write(green_gpio,1)
+	else:
+		pi.write(green_gpio,0)
 
 def sensorMeasuring():
-	GPIO.output(trigger_pin, GPIO.LOW)
-	#print('Waiting for the sensor to settle')
-	time.sleep(time_to_sleep);
-
-	GPIO.output(trigger_pin, GPIO.HIGH)
-	time.sleep(0.00001)
-
-	GPIO.output(trigger_pin, GPIO.LOW)
-
-	global pulse_start_time
-	global pulse_end_time
 	global spot_taken
 	global spot_free
 	global uuid_for_mongo
 
-	while GPIO.input(echo_pin) == 0:
-		pulse_start_time = time.time()
-	while GPIO.input(echo_pin) == 1:
-		pulse_end_time = time.time()
-
-	pulse_duration = pulse_end_time - pulse_start_time
-	measured_distance = round(pulse_duration * 17150, 2)
+	measured_distance = float(sonar.read()) / 100
 
 	if measured_distance <= desired_distance:
 		if not spot_taken:
@@ -111,16 +86,16 @@ def sensorMeasuring():
 				uuid_for_mongo = json.dumps(uuid.uuid4(), cls=UUIDEncoder).strip('\"')
 				print("UUID:",uuid_for_mongo)
 
-			GPIO.output(red_pin, GPIO.HIGH)
-			GPIO.output(green_pin, GPIO.LOW)
+			pi.write(red_gpio,1)
+			pi.write(green_gpio,0)
 			client.publish(car_is_here,json.dumps({ 'uuid' : uuid_for_mongo, 'spot' : spot_number, 'spot_status': True, 'sender' : 'pi', 'arrival' : datetime.now().strftime("%d-%m-%Y %H:%M:%S") }))
 			spot_taken = True
 			spot_free = False
 	else:
 		if not spot_free:
 			#print("Spot is free")
-			GPIO.output(green_pin, GPIO.HIGH)
-			GPIO.output(red_pin, GPIO.LOW)
+			pi.write(green_gpio,1)
+			pi.write(red_gpio,0)
 			client.publish(car_is_here,json.dumps({ 'uuid' : uuid_for_mongo, 'spot' : spot_number, 'spot_status': False, 'sender' : 'pi', 'departure' : datetime.now().strftime("%d-%m-%Y %H:%M:%S") }))
 			uuid_for_mongo = None
 			spot_free = True
@@ -135,6 +110,18 @@ if __name__ == '__main__':
 			#print(json.dumps(args))
 		try:
 			BROKER_ADDRESS = args['mqtt_ip']
+			trigger_gpio = args['sensor_trigger']
+			echo_gpio = args['sensor_echo']
+			red_gpio = args['red_gpio']
+			green_gpio = args['green_gpio']
+			yellow_gpio = args['yellow_gpio']
+			blue_gpio = args['blue_gpio']
+			desired_distance = float(args['desired_distance'])
+			time_to_sleep = float(args['time_to_sleep'])
+			spot_number = args['id_number_of_sensor']
+			car_is_here = args['topics'][0]
+			image_is_taken = args['topics'][1]
+
 			client = mqtt.Client('pi_sensors')
 			client.on_connect = on_connect
 			client.on_message = on_message
@@ -142,41 +129,31 @@ if __name__ == '__main__':
 			print("Connecting to MQTT broker:",BROKER_ADDRESS)
 			client.loop_start()
 
-			trigger_pin = args['sensor_trigger']
-			echo_pin = args['sensor_echo']
-			red_pin = args['red_pin']
-			green_pin = args['green_pin']
-			yellow_pin = args['yellow_pin']
-			blue_pin = args['blue_pin']
-			desired_distance = float(args['desired_distance'])
-			time_to_sleep = float(args['time_to_sleep'])
-			spot_number = args['id_number_of_sensor']
-
-			car_is_here = args['topics'][0]
-			image_is_taken = args['topics'][1]
-
-			pulse_start_time = 0.0
-			pulse_end_time = 0.0
 			spot_taken = False
 			spot_free = False
 			uuid_for_mongo = None
 
+			pi = pigpio.pi()
+			sonar = sonar_ranger.ranger(pi, trigger_gpio, echo_gpio)
 			blink_speed = float(args['blink_speed'])
-			blink_thread = myThread(1,"alpr_recognition",yellow_pin,blue_pin)
+			blink_thread = myThread(1,"alpr_recognition")
 			blink_thread.start()
+
+			setToDefault(True)
 
 			if len(sys.argv) > 1:
 				uuid_for_mongo = sys.argv[1]
 
-			pinsInitialization()
 			while True:
-				time.sleep(0.01)
 				sensorMeasuring()
+				time.sleep(time_to_sleep);
 
 		finally:
-			GPIO.cleanup()
 			blink_thread.thread_working = False
 			blink_thread.join()
+			sonar.cancel()
+			setToDefault(False)
+			pi.stop()
 
 	except (Exception, KeyboardInterrupt) as ex:
 		template = "An exception of type {0} occurred. Arguments:\n{1!r}"
@@ -184,9 +161,6 @@ if __name__ == '__main__':
 		sys.stderr.write(message)
 		try:
 			print("Trying to exit with sys.exit(0)")
-			blink_thread.thread_working = False
-			blink_thread.join()
-			GPIO.cleanup()
 			sys.exit(0)
 		except SystemExit:
 			print("Sys.exit(0) did not work, trying to exit with os.exit(0)")
